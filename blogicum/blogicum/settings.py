72148ac3a842a44cff0435aacd2577fb2fca10d5
k9607 <k9607492884@gmail.com>
2026-08-13 00:10:33 +0300

    Переход из SQLite в postgreSQL

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = "blogicum.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'postgrekiller321',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
